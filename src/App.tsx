bc265616bff82f84f0acb57ab8e21653d719ac6d
Faukkirik <kote_33419@mai.ru>
2023-01-14 23:04:29 +0300

    1 commit-useState and uncontrolled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/accordion/accordion";
+import {Rating} from "./components/rating/Rating";
+import {OnOff} from "./components/OnOff/OnOff";
+import {UnControlledAccordion} from "./components/accordion/uncontrolledaccordion";
+import {UnControlledRating} from "./components/rating/UnControlledRating";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log("App rendering")
+    return (
+        <div className={"App"}>
+            {/*<PageTitle title={"This is APP components"}/>
+            <PageTitle title={"My friends"}/>
+            Article 1
+            <Rating value={3}/>*/}
+            {/*<Accordion titleValue={"Menu"} collapsed={true}/>
+            <Accordion titleValue={"Users"} collapsed={false}/>*/}
+            {/*Article 2
+            <Rating value={4}/>
+            Score...*/}
+            {/*<Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>*/}
+            <div><OnOff /></div>
+        <UnControlledAccordion titleValue={"--Menu--"}/>
+            <UnControlledRating />
+        </div>
+    );
 }
+type PageTitlePropsType = {
+    title: string;
+}
+function PageTitle(props: PageTitlePropsType) {
+
+    console.log("PageTitle rendering")
+    return (<div><b>
+        {props.title}
+    </b>
+    </div>)
+}
+
 
 export default App;
